359eb7d3809c851fbb92383ea34c8b85f8b0b58e
syahrulfajriansyah <syahrulisbani1@gmail.com>
2025-05-13 03:13:01 +0700

    Last Up to Date

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('login', [AuthController::class, 'login']);
 Route::get('register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::post('register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'logout'])->name('logout');
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 // -------------------Admin--------------------------
 Route::prefix('admin')->middleware('auth')->group(function () {
